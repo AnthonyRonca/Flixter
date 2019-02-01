425eb8fb14e72a6e7fe67fe805d297c7a4e40b45
ANTHONY RONCA <5003400488@wce111-02.local>
2019-01-31 19:14:17 -0800

    Initial Commit

---
+++ Flixter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixter
+//
+//  Created by ANTHONY RONCA on 1/31/19.
+//  Copyright © 2019 ANTHONY RONCA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
